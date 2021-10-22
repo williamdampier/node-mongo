f1821c897ee8b86293ebe459830fe86cad533784
williamdampier <74879032+williamdampier@users.noreply.github.com>
2021-10-22 13:55:32 +1300

    CRUD implemented

--- PostController.js
+++ PostController.js
@@ -3,15 +3,22 @@ import Post from "./Post.js";
 class PostController {
     async getAll(req,res){
             try {
-
+                const posts = await Post.find();
+                return res.json(posts);
             }
             catch (e) {
                 res.status(500).json(e);
             }
     }
 
-    async getOne(){
+    async getOne(req, res){
         try {
+            const {id} = req.params;
+            const post = await Post.findById(id);
+            if (!id) { 
+                res.status(400).json({message: "id not specified"});
+            }
+            return res.json(post);
 
         }
         catch (e) {
@@ -30,18 +37,28 @@ class PostController {
         }
     }
 
-    async updatePost(){
+    async updatePost(req, res){
         try {
-
+            const post = req.body;
+            if (!post._id) { 
+                return res.status(400).json({message: "id not specified"});
+            }
+            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new:true});
+            res.json(updatedPost);
         }
         catch (e) {
             res.status(500).json(e);
         }
     }
 
-    async deletePost() {
+    async deletePost(req, res) {
         try {
-
+            const {id} = req.params;
+            const deletedPost = await Post.findByIdAndRemove(id);
+            if (!id) { 
+                res.status(400).json({message: "id not specified"});
+            }
+            return res.status(200).json({message: `post with id: ${id} deleted`});
         }
         catch (e) {
             res.status(500).json(e);
